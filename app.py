952bec8d0d43d5cd2b27dc70ae2db0015dc5c366
José Luiz Ferreira <jlfsjunior@gmail.com>
2021-06-17 22:00:23 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_chakraui_components as dxc
+import plotly.express as px
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+app.title = "Just one more a Dash app in GCP..."
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = dxc.Box(children=[
+    dxc.Box(children='''
+        This is a place for exploration. Build with ❤️ and dash-chakraui-components.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port="8080")
